b8bf98f52369854cedb24b1d0ab6115a75d665bf
Priyanka Arora <parora0711@gmail.com>
2025-01-02 15:49:17 +0530

    Changes

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
-import Card from './Card'
+import React from 'react';
+import Card from './Card';
+import { useRef } from 'react';
 
 function Foreground() {
+  const ref = useRef(null);
+  const data = [
+    {
+      desc: "Lorem ipsum dolor sit amet jdnmas sxjghfsk",
+      filesize: ".9mb",
+      close: true,
+      tag: { isOpen : true, tagTitle: "Download Now", tagColor: "green" },
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet jdnmas sxjghfsk",
+      filesize: ".9mb",
+      close: true,
+      tag: { isOpen : true, tagTitle: "Download Now", tagColor: "blue" },
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet jdnmas sxjghfsk",
+      filesize: ".9mb",
+      close: true,
+      tag: { isOpen : true, tagTitle: "Download Now", tagColor: "green" },
+    },
+  ];
   return (
-    <div>
       <>
-      <div className='fixed top-0 left-0 z-[3] w-full h-full'></div>
-      <Card/>
+      <div ref={ref} className='fixed top-0 left-0 z-[3] w-full h-full flex gap-5 flex-wrap p-5'>
+        {data.map((item,index)=>(
+          <Card data={item} reference={ref} />
+        ))}
+      </div>
       </>
-    </div>
   )
 }
 
